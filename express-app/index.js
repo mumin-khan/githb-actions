2077709e6df56e46bccccf0f5dc3bb7520d50db6
mumin-khan <muminkhan2000@gmail.com>
2023-11-25 08:43:22 +0530

    action test

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello dunya!</h1>')
+  res.send('<h1>Hello dunya fanii!</h1>')
 })
 
 const PORT = 8080
